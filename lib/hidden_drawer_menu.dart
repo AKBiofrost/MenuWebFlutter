fc2473bb816820c1df981ca0e8f2ed758cfd6cf4
Bivfrost <cedt00@gmail.com>
2022-12-17 21:54:38 +0100

    esqueleto y login

---
+++ lib/hidden_drawer_menu.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:hidden_drawer_menu/hidden_drawer_menu.dart';
+import 'package:menuweb/src/page/Menu.dart';
+import 'package:menuweb/src/page/first.dart';
+import 'package:menuweb/src/page/home.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+      home: Home(),
+    );
+  }
+}
+
+class HiddenDrawer extends StatefulWidget {
+   const HiddenDrawer({Key? key}) : super(key: key);
+
+  @override
+  State <HiddenDrawer> createState() => _HiddenDrawerState();
+
+}
+
+class _HiddenDrawerState extends State<HiddenDrawer> {
+
+  List<ScreenHiddenDrawer> _page=[];
+  
+  void initState(){
+  
+    super.initState(); 
+    
+    _page = [
+      ScreenHiddenDrawer(
+        ItemHiddenMenu(
+          name:'MenuWeb ',
+          baseStyle: TextStyle( fontWeight: FontWeight.bold,
+              color: Colors.white
+          ),
+            selectedStyle: TextStyle(),
+          colorLineSelected: Colors.white
+        ),
+        Home(),
+
+      ),
+
+      ScreenHiddenDrawer(
+        ItemHiddenMenu(
+          name:'Menu ',
+          baseStyle: TextStyle( fontWeight: FontWeight.bold,
+              color: Colors.white
+          ),
+          selectedStyle: TextStyle(),
+            colorLineSelected: Colors.white
+        ),
+        Menu(),
+      ),
+      ScreenHiddenDrawer(
+        ItemHiddenMenu(
+          name:'clientes',
+          baseStyle: TextStyle( fontWeight: FontWeight.bold,
+          color: Colors.white
+          ),
+          selectedStyle: TextStyle(),
+            colorLineSelected: Colors.white
+
+        ),
+        FirstPage(),
+      ),
+
+  ];
+    }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return HiddenDrawerMenu(
+        screens: _page,
+        backgroundColorMenu: Colors.deepPurpleAccent.shade400 ,
+        initPositionSelected: 0,
+      slidePercent: 35,
+      contentCornerRadius: 40,
+      boxShadow: [],
+
+    );
+
+  }
+    
+  }
+
---
+++ lib/src/page/Menu.dart
@@ -0,0 +1,25 @@
+
+import 'package:flutter/material.dart';
+
+
+class Menu extends StatefulWidget{
+  const Menu({Key? key}): super(key:key);
+
+  @override
+  State<Menu> createState()=>_MenuState();
+
+}
+
+class _MenuState extends State <Menu>{
+
+  @override
+
+  Widget build(BuildContext context){
+
+    return const Scaffold(  body: Center(
+      child: Text('segunda  pagina'),
+    ),
+      backgroundColor: Colors.white,
+    );
+  }
+}
