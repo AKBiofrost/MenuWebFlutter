640521772d7a7c552848492b88c25e82297bbd03
Bivfrost <cedt00@gmail.com>
2022-12-18 02:39:19 +0100

    actionbar

--- lib/src/page/Menu.dart
+++ lib/src/page/Menu.dart
@@ -8,18 +8,50 @@ class Menu extends StatefulWidget{
   @override
   State<Menu> createState()=>_MenuState();
 
+
+
 }
 
 class _MenuState extends State <Menu>{
+int _selectedIndex=0;
+
+  void _navigateBottombar (int index){
+
+    setState(() {
+        _selectedIndex=index;
+    });
+  }
 
   @override
 
   Widget build(BuildContext context){
 
-    return const Scaffold(  body: Center(
+    return  Scaffold(
+      body: Center(
       child: Text('segunda  pagina'),
     ),
       backgroundColor: Colors.white,
+
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        type:BottomNavigationBarType.fixed,
+        onTap: _navigateBottombar,
+        items:[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'principal',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.message),
+            label: 'mensaje',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'persona',
+          ),
+        ],
+      ),
+
     );
   }
 }
